b33c2509dea7ee13015f2cee84cd9755fe2716c3
Santigil0814 <santigil0814gmail.com>
2021-10-21 22:10:58 -0500

    Avance en el sena

--- src/components/Captura_boddy/Captura_boddy.js
+++ src/components/Captura_boddy/Captura_boddy.js
@@ -31,12 +31,11 @@ const Captura_boddy = () => {
       ]
 
     return (
-
             <Container className="Boddy_captura">
                 <Row>
                     <Col sm={4}>
-                        <button className="btn_captura" onClick={() =>History.push('/Ingresar')}>Capturar info.</button>
-                        <button className="btn_inv" onClick={() =>History.push('/Ingresar')}>Iventario</button>
+                        <button className="btn_captura" onClick={() =>History.push('/Captura_info')}>Capturar info.</button>
+                        <button className="btn_inv" onClick={() =>History.push('/Ingresar')}>Inventario</button>
                     </Col>
                     <Col sm={8}>
                         <Container className="contenedor">
@@ -44,15 +43,36 @@ const Captura_boddy = () => {
                                 <Col>
                                 <br></br>
                                 <Table striped bordered hover size="sm">
-                                    <tbody>
-                                        {datadata.map(index => {
-                                            return (
-                                                <tr>
-                                                    <td>{index.Maquinaria1}</td>
-                                                </tr>
-                                            );
-                                        })}
-                                    </tbody>
+                                <thead>
+                                    <tr>Tipo maquinaria:</tr>
+                                    <tr>Número interno:</tr>
+                                    <tr>Horómetro:</tr>
+                                    <tr>Fecha/Hora ingreso:</tr>
+                                    <>
+                                      <Form.Select className="menu-desplegable">
+                                        <option>Tipo de mantenimiento:</option>
+                                        <option value="1">Preventivo</option>
+                                        <option value="2">Predictivo</option>
+                                        <option value="3">Correctivo</option>
+                                        <option value="4">Proactivo</option>
+                                      </Form.Select>
+                                      <Form.Select className="menu-desplegable">
+                                        <option>Secciones que ingresa:</option>
+                                        <option value="1">Diesel</option>
+                                        <option value="2">Automotriz</option>
+                                        <option value="3">Lámina y pintura</option>
+                                        <option value="4">Eléctricos</option>
+                                        <option value="5">Soldadura</option>
+                                        <option value="6">Mecánicos campo</option>
+                                        <option value="7">Torno</option>
+                                        <option value="8">Serviteca</option>
+                                        <option value="9">Montallantas</option>
+                                      </Form.Select>
+                                    </>
+                                    <tr>Ficha operador:
+                                    </tr>
+                                    <tr>Nombre operador:</tr>
+                                </thead>
                                 </Table>
                                 <br></br>
                                 </Col>
@@ -60,8 +80,10 @@ const Captura_boddy = () => {
                                 <>
                                 <br></br>
                                     <p>Observaciones:</p>
-                                    <Form.Control size="sm" type="text" placeholder="" />
-                                    <br></br>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                      <Form.Label>Example textarea</Form.Label>
+                                      <Form.Control as="textarea" rows={3} />
+                                      </Form.Group>
                                     <button className="btn_solicitud" onClick={() =>History.push('/Ingresar')}>Solicitar aprobación ingreso</button>
                                     <button className="btn_genCorreo" onClick={() =>History.push('/Ingresar')}>Generar correo</button>
                                 </>
