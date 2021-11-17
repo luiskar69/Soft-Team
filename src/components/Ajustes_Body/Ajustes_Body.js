0443448c4a9e099280c33ad280e5b304caf2ff7b
Santigil0814 <santigil0814@gmail.com>
2021-11-17 16:21:29 -0500

    avance

--- src/components/Ajustes_Body/Ajustes_Body.js
+++ src/components/Ajustes_Body/Ajustes_Body.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import Config from './MR.jpg'
 
 const Ajustes_Body = () => {
     return (
         <div>
-            < Image src={Config} roundedCircle
-              width="90"
-              height="90"
-           />
+            
         </div>
     )
 }
